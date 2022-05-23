ca41abf74447c1c7ee50496f490f99f120974d39
Agustin Pelle <agustinpelle@hotmail.com>
2022-05-23 17:05:22 -0300

    efdfsd

---
+++ familia/models.py
@@ -0,0 +1,9 @@
+from concurrent.futures.process import _MAX_WINDOWS_WORKERS
+from django.db import models
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    edad = models.IntegerField(null=True)
+    fecha_nacimiento = models.DateField()
---
+++ familia/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.template import loader
+from django.shortcuts import render
+from familia.forms import PersonaForm
+
+from familia.models import Persona
+
+def index(request):
+    personas = Persona.objects.all()
+    template = loader.get_template('familia/lista_familiares.html')
+    context = {
+        'personas': personas,
+    }
+    return HttpResponse(template.render(context, request))  
+
+
+def agregar(request):
+    '''
+    TODO: agregar un mensaje en el template index.html que avise al usuario que 
+    la persona fue cargada con éxito
+    '''
+
+    if request.method == "POST":
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+
+            nombre = form.cleaned_data['nombre']
+            apellido = form.cleaned_data['apellido']
+            email = form.cleaned_data['email']
+            edad = form.cleaned_data['edad']
+            fecha_nacimiento = form.cleaned_data['fecha_nacimiento']
+            Persona(nombre=nombre, apellido=apellido, email=email, fecha_nacimiento=fecha_nacimiento, edad=edad).save()
+
+            return HttpResponseRedirect("/")
+    elif request.method == "GET":
+        form = PersonaForm()
+    else:
+        return HttpResponseBadRequest("Error no conzco ese metodo para esta request")
+
+    
+    return render(request, 'familia/form_carga.html', {'form': form})
+
+
+def borrar(request, identificador):
+    '''
+    TODO: agregar un mensaje en el template index.html que avise al usuario que 
+    la persona fue eliminada con éxito        
+    '''
+    if request.method == "GET":
+        persona = Persona.objects.filter(id=int(identificador)).first()
+        if persona:
+            persona.delete()
+        return HttpResponseRedirect("/")
+    else:
+        return HttpResponseBadRequest("Error no conzco ese metodo para esta request")
+
+
+def actualizar(request, identificador):
+    '''
+    TODO: implementar una vista para actualización
+    '''
+    pass
